9dbb40fd3c0aa439588607a6db477d18016f1da9
MazvydasV <mvaicekauskas@inbox.lt>
2021-12-23 19:15:15 +0200

    tabs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // about section tab navigation
 const tabNavigationElements = document.querySelectorAll(".tabs .tab-navigation li");
+const tabContentElements = document.querySelectorAll(".tabs .tab-content");
 
 function removeActiveClassesFromElements(elements) {
     for (let element of elements) {
@@ -9,8 +10,13 @@ function removeActiveClassesFromElements(elements) {
 
 for (let tabElement of tabNavigationElements) {
    tabElement.addEventListener("click", function() {
+    const target = this.dataset.target;
+
     removeActiveClassesFromElements(tabNavigationElements);
        this.classList.add("active");
+
+       removeActiveClassesFromElements(tabContentElements);
+       document.querySelector('.tab-content[data-tab="'+target+'"]').classList.add('active');
     });
 }
 
